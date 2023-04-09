d050534ce35d627849735274d134438f49640628
K2976 <120504065+K2976@users.noreply.github.com>
2023-04-09 12:17:15 +0530

    Add files via upload

---
+++ area of rectangle.py
@@ -0,0 +1,5 @@
+#print area of triangle
+x = int(input("Length = "))
+y = int(input("Breadth = "))
+z = x*y
+print("Area of Triangle ->",z)
---
+++ table of 5.py
@@ -0,0 +1,4 @@
+#to display the table of any given no.
+a = int(input("Given no. = "))
+for i in range(1,11):
+    print(a,"*",i,'=',a*i)
